5c9c515ea103b885eb38be60ae85de04fc07cb72
wch-os <wch1224.s@gmail.com>
2025-03-17 16:08:03 +0900

    chore(board): Add dependency. board-api → board-application

--- board/api/build.gradle.kts
+++ board/api/build.gradle.kts
@@ -1,21 +1,27 @@
 plugins {
     id("java")
+    id("java-library")
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
+    // modules
+    api(project(":board:application"))
+
     // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.data:spring-data-commons")
 
     // lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
+
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
