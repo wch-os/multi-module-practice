961d3dcc230f20a2e5c1853321caee2550527697
wch-os <wch1224.s@gmail.com>
2025-03-17 23:31:52 +0900

    feat(board): create "board/commons" module
    - move from 'board/application' to 'board/commons'

--- board/common/build.gradle.kts
+++ board/common/build.gradle.kts
@@ -11,21 +11,14 @@ repositories {
 
 dependencies {
     // modules
-    implementation(project(":board:commons"))
+    implementation(project(":common"))
 
     // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.data:spring-data-commons")
 
     // lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
-
-    // mapstruct
-    implementation("org.mapstruct:mapstruct:1.6.3")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
